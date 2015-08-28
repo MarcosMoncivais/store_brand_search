37139ab4875a4779027cc9bbb99ad9bd5e01aa63
Marcos Moncivais <marcos.moncivais@gmail.com>
2015-08-28 14:04:10 -0700

    add tests and methods for brand class

--- src/Brand.php
+++ src/Brand.php
@@ -32,6 +32,70 @@
 			$this->id = $GLOBALS['DB']->lastInsertId(); 
 		}
 
+		static function getAll()
 		
+		{
+		    $returned_brands = $GLOBALS['DB']->query("SELECT * FROM brands;");
+		    $brands = array();
+		    foreach ($returned_brands as $brand) {
+		        $id = $brand['id'];
+		        $name = $brand['name'];
+		        $new_brand = new Brand($id, $name);
+		        array_push($brands, $new_brand);
+		    }
+		    return $brands;
+		}
+		
+		static function deleteAll()
+		
+		{
+		    $GLOBALS['DB']->exec("DELETE FROM brands;");
+		    $GLOBALS['DB']->exec("DELETE FROM stores_brands;");
+		
+		}
+		
+		static function find($search_id)
+		
+		{
+		    $brands = Brand::getAll();
+		    $found_brand = null;
+		    foreach ($brands as $brand) {
+		        $brand_id = $brand->getId();
+		        if ($brand_id == $search_id) {
+		            $found_brand = $brand;
+		        }
+		    }
+		
+		    return $found_brand;
+		
+		}
+		
+		function addStore($new_store)
+		
+		{
+		    $GLOBALS['DB']->exec("INSERT INTO stores_brands (store_id, brand_id) VALUES ({$new_store->getId()}, {$this->getId()});");
+		}
+		
+		function getStores()
+		
+		{
+		    $query = $GLOBALS['DB']->query(
+		        "SELECT stores.* FROM
+		            brands JOIN stores_brands ON (brands.id = stores_brands.brand_id)
+		            JOIN stores ON (stores_brands.store_id = stores.id)
+		            WHERE brands.id = {$this->getId()};
+		        ");
+			    $stores = array();
+			    foreach ($query as $store) {
+			        $id = $store['id'];
+			        $name = $store['name'];
+			        $new_store = new Store($id, $name);
+			        array_push($stores, $new_store);
+		    }
+		
+		    return $stores;
+        }
+    }
+
 	}
 ?>
---
+++ tests/BrandTest.php
@@ -0,0 +1,156 @@
+<?php
+    
+        /**
+        * @backupGlobals disabled
+        * @backupStatic Attributes disabled
+        */
+
+
+        require_once "src/Brand.php";
+
+        require_once "src/Store.php";
+
+        $server = 'mysql:host=localhost;dbame=shoes_test';
+        $username = 'root';
+        $password = 'root';
+
+        $DB = new PDO($server, $username, $password);
+
+        class BrandTest extends PHPUnit_FrameworkTestCase
+        {
+            protected function tearDown()
+            {
+                Brand::deleteAll();
+                Store::deleteAll();
+            }
+
+            function testSave()
+            {
+                //Arrange
+                $id = null; 
+                $name = "Allen Edmonds";
+                $test_brand = new Brand($id, $name);
+
+                //Act
+                $test_brand->save();
+
+                //Assert
+                $result = Brand::getAll();
+                $this->assertEquals([$test_brand], $result);
+            }
+
+            function testDeleteAll()
+            {
+                //Arrange
+                $id = null;
+                $name = "Allen Edmonds";
+                $test_brand = new Brand($id, $name);
+
+                $name2 = "To Boot NY";
+                $test_brand2 = new Brand ($id, $name2);
+                $test_brand2->save();
+
+                //Act
+                Brand::deleteAll();
+
+                //Assert
+                $result = Brand::getAll();
+                $this->assertEquals([], $result);
+            }
+
+            function getAll()
+            {
+                //Arrange
+                $id = null;
+                $name = "Allen Edmonds";
+                $test_brand = new Brand($id, $name);
+                $test_brand->save();
+
+                $name2 = "Nike"
+                $test_brand2 = new Brand($id, $name2);
+                $test_brand2->save();
+
+                //Act
+                $result = Brand::getAll();
+
+                //Assert
+                $this->assertEquals([$test_brand, $test_brand2], $result);
+            }
+
+            function testFind()
+                
+                {
+                
+                //Arrange
+                
+                $id = null;
+                $name = "Nike";
+                $test_brand = new Brand($id, $name);
+                $test_brand->save();
+                
+                $name2 = "Adidas";
+                $test_brand2 = new Brand($id, $name2);
+                $test_brand2->save();
+                
+                //Act
+                
+                $result = Brand::find($test_brand2->getId());
+                
+                //Assert
+                
+                $this->assertEquals($test_brand2, $result);
+                }
+
+            function testAddStore()
+
+            {
+                //Arrange
+                
+                $id = null;
+                $name = "Nike";
+                $test_brand = new Brand($id, $name);
+                $test_brand->save();
+                
+                $name2 = "Super Shoes";
+                $test_store = new Store($id, $name2);
+                $test_store->save();
+                
+                //Act
+                
+                $test_brand->addStore($test_store);
+                
+                //Assert
+                
+                $result = $test_brand->getStores();
+                $this->assertEquals([$test_store], $result);
+                }
+            
+            function testGetStores()
+            
+            {
+                //Arrange
+                
+                $id = null;
+                $name = "Nike";
+                $test_brand = new Brand($id, $name);
+                $test_brand->save();
+                
+                $name2 = "Super Shoes";
+                $test_store = new Store($id, $name2);
+                $test_store->save();
+                
+                $name3 = "Nike Outlet";
+                $test_store2 = new Store($id, $name3);
+                $test_store2->save();
+                $test_brand->addStore($test_store);
+                $test_brand->addStore($test_store2);
+                
+                //Act
+                $result = $test_brand->getStores();
+                
+                //Assert
+                $this->assertEquals([$test_store, $test_store2], $result);
+            }
+                //methods being left out: update(), delete(),
+        }
+?>
